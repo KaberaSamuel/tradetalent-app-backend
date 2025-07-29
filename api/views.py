3d02df3c791730c8ef602a250303128eb881fa6c
KaberaSamuel <kaberanshutis@gmail.com>
2025-07-29 20:43:54 +0200

    basic api setup

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
