cc75f717eee9a365173d6fa739cab21d4c163006
KaberaSamuel <kaberanshutis@gmail.com>
2025-08-01 15:49:47 +0200

    added login funtionality

--- api/views.py
+++ api/views.py
@@ -17,3 +17,21 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class UserLoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        # Get user by email
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid Email'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check the password
+        if user.check_password(password): 
+            token, created = Token.objects.get_or_create(user=user)
+            serializer = UserSerializer(user)
+            return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid Password'}, status=status.HTTP_400_BAD_REQUEST)
