2a104516216b93059652a577c57a6fe68275f40e
KaberaSamuel <kaberanshutis@gmail.com>
2025-08-19 01:35:38 +0200

    used nested serialization to add serialized user data to retrieved listing data

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
+from users.serializers import HomeUserSerializer
 from .models import Listing
 
 class ListingSerializer(serializers.ModelSerializer):
+    user = HomeUserSerializer(read_only=True)
+
     class Meta:
         model = Listing
         fields = '__all__'
 
+    
     
