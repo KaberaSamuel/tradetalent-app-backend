415cc9b20caf6c5fed7bb4a2ed3ad9b0797faf53
KaberaSamuel <kaberanshutis@gmail.com>
2025-09-22 03:09:55 +0200

    updated cors to only allow origin set from environment variables

--- tradetalent_app_backend/settings.py
+++ tradetalent_app_backend/settings.py
@@ -145,8 +145,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cors configurations
-# CORS_ALLOWED_ORIGINS = [env("FRONTEND_URL")]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [env("FRONTEND_URL")]
 CORS_ALLOW_CREDENTIALS = True
 
 # Auth config
