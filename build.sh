d8f7d2756cfa544121cd6d91f6f5b3066be06650
KaberaSamuel <kaberanshutis@gmail.com>
2025-09-23 16:11:00 +0200

    updated build script to createsuperuser when necessary

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies 
 pip install pipenv
 pipenv install --deploy --system
 
+# Apply database migrations
 python manage.py migrate
+
+# Create superuser when specified
+if [ "$RUN_SUPERUSER_CREATION" = "True" ]; then
+    echo "Creating Django superuser using environment variables..."
+
+    python manage.py createsuperuser --noinput || true
+    
+    echo "Superuser creation command executed."
+fi
+
