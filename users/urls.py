6c7f929e7053e1295be82565064a0ca69a15596b
KaberaSamuel <kaberanshutis@gmail.com>
2025-09-30 06:44:39 +0200

    moved dynamic url in users.urls to bottom, so that it won't be matched before other routes

--- users/urls.py
+++ users/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("logout/", LogoutView.as_view()),
     path("request-password-reset/", RequestPasswordReset.as_view()),
     path("password-reset/", ResetPassword.as_view()),
-    path("<slug:slug>/", UserDetailView.as_view()),
     path("home/", HomeView.as_view()),
     path("", SearchView.as_view()),
+    path("<slug:slug>/", UserDetailView.as_view()),
 ]
