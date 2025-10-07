d93a26d95e591562fddd4b99642cea433c3d0e0f
KaberaSamuel <kaberanshutis@gmail.com>
2025-10-07 11:27:00 +0200

    added logic to get logged in user in the consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,26 @@
+from urllib.parse import parse_qs
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 from .models import Conversation
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from django.contrib.auth.models import AnonymousUser
 
 class ChatConsumer(JsonWebsocketConsumer):
     """
     This consumer is used to show user's online status,
     and send notifications.
     """
+
+    def authenticate_user_with_token(self, token):
+        try:
+            jwt_authentication = JWTAuthentication()
+            validated_token = jwt_authentication.get_validated_token(token)
+            user = jwt_authentication.get_user(validated_token)
+            return user
+        except (InvalidToken, TokenError):
+            return AnonymousUser()
 
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
@@ -16,6 +30,23 @@ class ChatConsumer(JsonWebsocketConsumer):
 
     def connect(self):
         self.user = self.scope["user"]
+
+        query_string = self.scope['query_string'].decode()
+        query_params = parse_qs(query_string)
+        token = query_params.get('token', [None])[0]
+        
+        if token:
+            if token:
+                user = self.authenticate_user_with_token(token)
+                print(user)
+                self.scope['user'] = user
+                if user.is_authenticated:
+                    self.accept()
+                else:
+                    return
+        else:
+            return
+        
         if not self.user.is_authenticated:
             return
 
