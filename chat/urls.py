8979cf6a4ed0a5a7e405cb23f073a5b1d4dce4f4
KaberaSamuel <kaberanshutis@gmail.com>
2025-10-12 09:13:26 +0200

    added logic for displaying active conversation

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MessageList
+from .views import MessageList, ConversationViewSet
 from .consumers import ChatConsumer
 
+
 urlpatterns = [
+    path("conversations/", ConversationViewSet.as_view({"get": "list"})),
     path("messages/", MessageList.as_view()),
     path("<slug>/", ChatConsumer.as_asgi()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,25 @@
 from rest_framework import generics
-from .models import Message
-from .serializers import MessageSerializer
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+from .models import Message, Conversation
+from .serializers import MessageSerializer, ConversationSerializer
 
 
 class MessageList(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     ordering = ("-timestamp",)
+
+
+class ConversationViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+    serializer_class = ConversationSerializer
+    queryset = Conversation.objects.none()
+    lookup_field = "name"
+
+    def get_queryset(self):
+        queryset = Conversation.objects.filter(name__contains=self.request.user.slug)
+        return queryset
+
+    def get_serializer_context(self):
+        return {"request": self.request, "user": self.request.user}
